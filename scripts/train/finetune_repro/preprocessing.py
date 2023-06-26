38cdc198acd72015377cb151a08f232bfa5ca24c
Mark Bastian <markbastian@gmail.com>
2023-06-26 16:57:29 -0600

    WIP

--- scripts/train/finetune_repro/preprocessing.py
+++ scripts/train/finetune_repro/preprocessing.py
@@ -6,6 +6,11 @@ def mbql(inp: Dict[str, str]) -> Dict[str, str]:
     desc = inp['description']
     ctx = inp['context']
     query = inp['query']
+    print('%' * 100)
+    print(desc)
+    print(ctx)
+    print(query)
+
     return {
         'prompt': json.dumps({'user_prompt': desc, 'context': ctx}),
         'response': query
