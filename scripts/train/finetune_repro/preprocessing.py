a44eba7b45920d3175976307fea76152bcff9145
Mark Bastian <markbastian@gmail.com>
2023-06-27 08:59:11 -0600

    WIP

--- scripts/train/finetune_repro/preprocessing.py
+++ scripts/train/finetune_repro/preprocessing.py
@@ -1,17 +1,20 @@
 import json
+import pprint
 from typing import Dict
 
+pp = pprint.PrettyPrinter(indent=2)
 
 def mbql(inp: Dict[str, str]) -> Dict[str, str]:
     desc = inp['description']
     ctx = inp['context']
     query = inp['query']
-    print('%' * 100)
-    print(desc)
-    print(ctx)
-    print(query)
 
-    return {
+    result = {
         'prompt': json.dumps({'user_prompt': desc, 'context': ctx}),
         'response': query
     }
+
+    print('%' * 100)
+    pp.pprint(result)
+
+    return result
